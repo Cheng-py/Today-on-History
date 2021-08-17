d6181befc98f4b0b15212f19f45bafda9d36a42e
Cheng-py <2238390982@qq.com>
2021-08-17 15:40:52 +0800

    The All

---
+++ API_.py
@@ -0,0 +1,12 @@
+from Scrapy_ import returnApi
+import json
+from flask import Flask,render_template,url_for,request
+import time
+app = Flask(__name__)
+datas = json.loads(returnApi())
+@app.route("/")
+def index():
+    return render_template('index2.html',datas=datas)
+
+if __name__ == '__main__':
+    app.run(debug="True",host="127.0.0.1",port='5001')
